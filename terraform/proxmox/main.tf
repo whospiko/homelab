e11dfcb006228ba0455339c98c798a717439c088
heng kakada <hengkakada899@gmail.com>
2026-09-19 00:42:23 +0700

    feat(terraform): create 3 instances of rocky 1 control plan and 2 worker node.

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,19 +1,79 @@
-module "test_vm" {
+module "cp-01" {
   source = "./modules/vm"
 
-  name           = "terraform-test-01"
+  name           = "cp-01"
   node_name      = var.proxmox_node
   vm_id          = 100
   template_vm_id = 9000
 
-  cores     = 2
-  memory    = 4096
-  disk_size = 20
+  tags = [ "cp", "homelab" ]
+
+  cores     = 4
+  memory    = 8192
+  disk_size = 40
 
   datastore_id   = "local-lvm"
   network_bridge = "vmbr0"
 
   ipv4_address = "192.168.100.100/24"
+  ipv4_gateway = "192.168.100.1"
+
+  dns_servers = [
+    "8.8.8.8",
+    "1.1.1.1"
+  ]
+  
+  username       = "rocky"
+  ssh_public_key = var.ssh_public_key
+}
+
+module "worker-01" {
+  source = "./modules/vm"
+
+  name           = "worker-01"
+  node_name      = var.proxmox_node
+  vm_id          = 200
+  template_vm_id = 9000
+
+  tags = [ "worker", "app","homelab" ]
+
+  cores     = 2
+  memory    = 8192
+  disk_size = 30
+
+  datastore_id   = "local-lvm"
+  network_bridge = "vmbr0"
+
+  ipv4_address = "192.168.100.200/24"
+  ipv4_gateway = "192.168.100.1"
+
+  dns_servers = [
+    "8.8.8.8",
+    "1.1.1.1"
+  ]
+  
+  username       = "rocky"
+  ssh_public_key = var.ssh_public_key
+}
+
+module "worker-99" {
+  source = "./modules/vm"
+
+  name           = "worker-99"
+  node_name      = var.proxmox_node
+  vm_id          = 999
+  template_vm_id = 9000
+
+  tags = [ "worker", "database","homelab" ]
+
+  cores     = 2
+  memory    = 4096
+  disk_size = 30
+
+  datastore_id   = "local-lvm"
+  network_bridge = "vmbr0"
+
+  ipv4_address = "192.168.100.99/24"
   ipv4_gateway = "192.168.100.1"
 
   dns_servers = [
--- terraform/proxmox/readme.md
+++ terraform/proxmox/readme.md
@@ -17,3 +17,13 @@ $token = 'terraform@pve!terraform=NEW_SECRET'
 curl.exe -k -i `
   -H "Authorization: PVEAPIToken=$token" `
   "https://192.168.100.10:8006/api2/json/access/permissions"
+
+
+
+// cp-01: 4cores, 8ram, 40GB (with rancher) (main control pla)
+
+// worker-01: 2cores, 8ram, 30GB (application)
+
+// worker-02: 2cores, 8ram, 40GB (application, monitoring) (Not Now)
+
+// worker-99: 2cores, 4ram, 30GB (database)
